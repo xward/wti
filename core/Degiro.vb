30291302a4690eb67eee00d61be37688e18c23a6
xwar <xavier@firefly.fr>
2024-08-29 21:33:49 +0200

    je catch process

--- core/Degiro.vb
+++ core/Degiro.vb
@@ -1,12 +1,13 @@
-﻿Namespace Degiro
+﻿Imports System.Reflection.Metadata
+Imports System.Runtime.InteropServices
+
+Namespace Degiro
 
 
 
     Module Degiro
-
         ' this is home page
         ' <Marché - Personal - Microsoft​ Edge>  process msedge
-
 
         ' https://trader.degiro.nl/trader/#/portfolio/assets
         ' <Portefeuille - Personal - Microsoft​ Edge>  process msedge
@@ -35,9 +36,42 @@
 
         Public handler As IntPtr
 
-        Public Sub pairToEdgeDegiro()
-            Dim p As Process = System.Diagnostics.Process.Start("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "https://trader.degiro.nl/login/fr#/login")
+        Public Const SWP_NOMOVE As Short = &H2
+        Public Const SWP_NOSIZE As Short = 1
+        Public Const SWP_NOZORDER As Short = &H4
+        Public Const SWP_SHOWWINDOW As Short = &H40
+        ReadOnly HWND_BOTTOM As IntPtr = New IntPtr(1)
 
+        <DllImport("user32.dll", EntryPoint:="SetWindowPos")>
+        Public Function SetWindowPos(
+ hWnd As IntPtr,
+ hWndInsertAfter As IntPtr,
+ x As Int32, y As Int32, cx As Int32, cy As Int32, wFlags As Int32) As IntPtr
+        End Function
+
+
+        Public Sub pairToEdgeDegiro()
+            ' Dim p As Process =
+            '   System.Diagnostics.Process.Start("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "https://trader.degiro.nl/login/fr#/login")
+
+            Pause(1000)
+            Dim Processes As Process() = Process.GetProcessesByName("msedge")
+
+            For Each p As Process In Processes
+
+                If Not p.MainWindowTitle.Contains("Personal - Microsoft​ Edge") Then Continue For
+
+                dbgMain.info("edge available: <" & p.MainWindowTitle & "> process " & p.ProcessName & " (" & p.Id & ") " & User32.getWindowPos(p.MainWindowHandle).ToString)
+
+            Next
+
+
+            'Dim handle As IntPtr = p.MainWindowHandle
+            'dbgMain.info(handle.ToString)
+
+            'SetWindowPos(handle, HWND_BOTTOM, 200, 200, 0, 0, SWP_NOZORDER Or SWP_NOSIZE Or SWP_SHOWWINDOW)
+
+            Exit Sub
 
             'https://learn.microsoft.com/en-us/answers/questions/378971/vb-net-and-the-windows-10-microsoft-edge-browser
             'Dim procs As Process() = Process.GetProcessesByName("Microsoft Edge")
@@ -69,30 +103,32 @@
 
 
             ' first time found
-            If Not p Is Nothing And degiroState = DegiroStateEnum.UNKNOWN Then
+            'If Not p Is Nothing And degiroState = DegiroStateEnum.UNKNOWN Then
 
-                dbgMain.info(p.MainWindowTitle)
+            '    dbgMain.info(p.MainWindowTitle)
 
-                'windowHandle = User32.FindWindow(Nothing, windowTitleName)
-                'windowPaired = windowHandle <> Nothing And windowHandle.ToInt32 > 0
+            '    Dim handler As IntPtr = 14908 ' p.MainWindowHandle
 
-                handler = User32.FindWindow(Nothing, p.MainWindowTitle)
-                '  handler = User32.FindWindow(Nothing, "Marché")
+            '    'windowHandle = User32.FindWindow(Nothing, windowTitleName)
+            '    'windowPaired = windowHandle <> Nothing And windowHandle.ToInt32 > 0
 
-
-
-                dbgMain.info("Degiro window handler " & p.Id & " " & handler.ToString & " rect=" & User32.getWindowPos(handler).ToString)
-                User32.bringToFront(handler)
-
-            End If
+            '    '     handler = User32.FindWindow(Nothing, p.MainWindowTitle)
+            '    '  handler = User32.FindWindow(Nothing, "Marché")
 
 
-            If p Is Nothing Then
-                degiroState = DegiroStateEnum.NO_EDGE
-            Else
 
-                degiroState = DegiroStateEnum.EDGE_PAIRED
-            End If
+            '    dbgMain.info("Degiro window handler " & p.Id & " " & handler.ToString & " rect=" & User32.getWindowPos(handler).ToString)
+            '    User32.bringToFront(handler)
+
+            'End If
+
+
+            'If p Is Nothing Then
+            '    degiroState = DegiroStateEnum.NO_EDGE
+            'Else
+
+            '    degiroState = DegiroStateEnum.EDGE_PAIRED
+            'End If
 
 
 
---
+++ core/tools/Pause.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Threading
+
+Module TimeHelper
+    Private randomO As New Random
+
+    Public Function rand(ByVal range As Integer) As Integer
+        Return CInt(Math.Floor(randomO.NextDouble() * (range + 1)))
+    End Function
+
+
+    Public doStopCurrentPause As Boolean = False
+    Public Sub RandPause(ByVal elapseMin As Integer, ByVal elapseMax As Integer, Optional ByVal doPrintElasped As Boolean = False)
+        Dim p As Integer = rand(elapseMax - elapseMin) + elapseMin
+        Pause(p, doPrintElasped)
+    End Sub
+
+
+    Public Sub Pause(ByVal elapse As Integer, Optional ByVal doPrintElasped As Boolean = False)
+        If elapse = 0 Then Exit Sub
+        If elapse < 501 Then
+            Thread.Sleep(elapse)
+            Exit Sub
+        End If
+
+        Dim startingPause As Date = Now
+
+        While (Now.Subtract(startingPause).TotalMilliseconds < elapse)
+            Thread.Sleep(35)
+
+            'print elasped in a label
+            If doPrintElasped And Now.Subtract(startingPause).TotalSeconds > 1 Then
+                '  FrmDashBoard.Label1STR = "Pause while " & Now.Subtract(startingPause).ToString
+            End If
+
+            'If FrmDashBoard.b0tRunning = False Then
+            '    If Not (FrmDashBoard.tPlay Is Nothing) Then
+            '        If FrmDashBoard.tPlay.IsAlive Then
+            '            ' If kmOutMode = "rs232" Then UsbKMRS232.releaseAllKeys() ' @todo: close if port was opened
+            '            FrmDashBoard.tPlay.Abort()
+            '        End If
+            '    End If
+            '    Exit Sub
+            'End If
+            If doStopCurrentPause Then
+                doStopCurrentPause = False
+                Exit Sub
+            End If
+        End While
+
+    End Sub
+
+    ' ex to wait next 03h45 timeDate="XX/XX/XXXX 03:45:XX"
+    Public Sub waitUntilTimeDate(ByVal timeDate As String, Optional ByVal doPrintElasped As Boolean = False)
+        Dim over As Boolean = False
+        Dim st As String
+        Dim a, b As String
+
+        While Not over
+            Thread.Sleep(750)
+            st = Now.ToString  ' 20/05/2014 13:05:12
+
+            over = True
+            For nu = 0 To st.Length - 1
+                a = st.Substring(nu, 1)
+                b = timeDate.Substring(nu, 1)
+                If b = "X" Then Continue For
+                If a <> b Then over = False
+            Next
+
+            'print elasped in a label
+            'If doPrintElasped Then
+            '    FrmDashBoard.Label1STR = "Pause until " & timeDate & " now is " & Now.ToString
+            'End If
+
+        End While
+
+
+    End Sub
+
+End Module
+
