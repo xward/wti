59dd7ca3c979f81b753877c4be8c2477eb90e5dd
xwar <xavier@firefly.fr>
2024-09-07 08:00:10 +0200

    n

--- pull_rebuild_start.bat
+++ pull_rebuild_start.bat
@@ -11,6 +11,7 @@ SET SLN=WorstTradingInitiative.sln
 SET MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe
 "%MSBUILD%" "%SLN%" /p:Configuration=Debug
 
+:: we change path to have WorstTradingInitiative.exe output everything in bin\Debug\net8.0-windows folder (ex: ester.txt)
 CD bin\Debug\net8.0-windows
 
 WorstTradingInitiative.exe
